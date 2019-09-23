4097b0e6a3aafe52d4e4b9f0fdffb3e25ddfa55f
cjslack <40322960+cjslack@users.noreply.github.com>
2019-09-23 14:46:39 -0400

    formatting

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -5,7 +5,8 @@ function About() {
         <div style={{ padding: '1em 2em' }}>
             <h2>About</h2>
             <p>This is my first React app. I made this using create-react-app as a template. All components are functional components.</p>
-            <a href='https://github.com/cjslack/React-Todo-List'>View Code on GitHub</a>
+            <br />
+            <a href='https://github.com/cjslack/React-Todo-List' style={{ color: '#333', textDecoration: 'underline'}}>View Code on GitHub</a>
         </div>
     )
 }
